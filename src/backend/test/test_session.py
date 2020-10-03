a24fe945299a0f5f4d4abc926cf4f154a970a7c1
Daniel A <den.xyz@pm.me>
2020-10-03 18:17:10 +0100

    Added working api key test and set warnings to ignore unclosed sslsocket warning

--- src/backend/test/test_session.py
+++ src/backend/test/test_session.py
@@ -1,22 +1,27 @@
+import warnings
 import unittest
+import os
 from badpackets.session import BadPacketsSession
 
 class TestStringMethods(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.api_url = "https://api.badpackets.net/v1/"
-        cls.api_key = "enter_api_key"
-        cls.bp_auth_session = BadPacketsSession(cls.api_url, cls.api_key)
-        cls.bp_unauth_session = BadPacketsSession(cls.api_url)
+        cls.api_url = os.getenv("BADPACKETS_API_URL")
+        cls.api_key = os.getenv("BADPACKETS_API_TOKEN")
+        cls.session = BadPacketsSession(cls.api_url, cls.api_key)
+    
+    def setUp(self):
+        warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<ssl.SSLSocket.*>") 
 
-    def test_ping(self):
-        response = self.bp_unauth_session.get('/ping')
-        self.assertEqual(response.status_code, 200)
-
-    def test_unauthenticated(self):
-        response = self.bp_unauth_session.get('/query?tags=Mirai')
+    def test_bad_api_key(self):
+        bad_key_session = BadPacketsSession(self.api_url, "some_bad_key")
+        response = bad_key_session.get('ping')
         self.assertEqual(response.status_code, 403)
+    
+    def test_working_api_key(self):
+        response = self.session.get('ping')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
