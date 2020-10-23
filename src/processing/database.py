80ca7bfb0fd13dd1625bbbcf886e2c7b3dde4f14
Daniel A <den.xyz@pm.me>
2020-10-23 13:08:52 +0100

    Added database.py, includes some database models for processing data

---
+++ src/processing/database.py
@@ -0,0 +1,68 @@
+from mongoengine import *
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+
+load_dotenv(verbose=True)
+
+connect(host=os.getenv("MONGODB_URL"))
+
+class BadPacketsResult(Document):
+    """
+    BadPackets Result JSON information
+    """
+    event_id = StringField(required=True, primary_key=True, unique=True)
+    source_ip_address = StringField(required=True)
+    country = StringField(required=True, max_length=4)
+    user_agent = StringField(required=True)
+    payload = StringField(required=True)
+    post_data = StringField(required=True)
+    target_port = IntField(required=True)
+    protocol = StringField(required=True)
+    event_count = IntField(required=True)
+    first_seen = DateTimeField(required=True)
+    last_seen = DateTimeField(required=True)
+
+    meta = {
+        "indexes": ["event_id"],
+    }
+
+
+class BadPacketsIPAddress(Document):
+    """
+    Server/IP Address information pulled from payload data of BadPackets results
+    """
+    ip_address = StringField(required=True, primary_key=True, unique=True)
+    ip_geolocation = StringField(required=True)
+    domain = StringField()
+    server_type = StringField(required=True, choices=["C2", "Loader", "Report", "Bot"])
+    last_seen = DateTimeField(default=datetime.utcnow)
+    vt_ip_url = URLField()
+
+    meta = {
+        "indexes": ["ip_address", "server_type"],
+    }
+
+
+class BadPacketsPayload(Document):
+    """
+    Malware payload information retrieved from BadPackets results
+    """
+    bp_event_id = StringField(required=True, primary_key=True, unique=True)
+    ip_address = StringField(required=True)
+    payload_url = URLField(required=True)
+    virustotal_url = URLField(required=True)
+
+    meta = {
+        "indexes": ["bp_event_id", "ip_address"],
+    }
+
+
+class BadPacketsTag(Document):
+    """
+    BadPackets tag for a given API result
+    """
+    bp_event_id = StringField(required=True, primary_key=True, unique=True)
+    cve = StringField()
+    category = StringField()
+    description = StringField()
