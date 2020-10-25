045538459b61f85810aad37913bbbbf92df511d0
Daniel A <den.xyz@pm.me>
2020-10-25 18:39:30 +0000

    Added endpoint /api/geolocations which returns array of all geolocations stored within last 24h

--- src/backend/routes/api/geolocations.js
+++ src/backend/routes/api/geolocations.js
@@ -1,12 +1,23 @@
 "use strict"
+const mongoose = require("mongoose")
 const express = require("express")
 
 let router = express.Router()
+let connection = mongoose.connection
 
 router
   .route("/")
   .get(async (req, res) => {
-    console.log(req.session)
+    let twentyFourHoursAgo = new Date()
+    twentyFourHoursAgo.setHours(twentyFourHoursAgo.getHours() - 24)
+
+    connection.db.collection("geo_data", function(err, collection){
+        collection
+        .find({"updated_at": {"$gte": twentyFourHoursAgo}})
+        .toArray(function(err, data){
+            res.json(data)
+        })
+    })
   })
 
 module.exports = router
