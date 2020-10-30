783a85110671b4cf5e72f12016b05d34b7591ff2
Daniel A <den.xyz@pm.me>
2020-10-30 17:00:59 +0000

    Added documentation to main.py and moved parser.add_options into function

--- src/processing/main.py
+++ src/processing/main.py
@@ -19,38 +19,47 @@ BP_KEY = os.getenv("BADPACKETS_API_KEY")
 # Optopn Parser setup
 parser = optparse.OptionParser()
 
-parser.add_option(
-    "-t",
-    "--threads",
-    action="store",
-    dest="threads",
-    help="The number of worker threads to use while processing results.",
-    default=4,
-    type=int
-)
 
-parser.add_option(
-    "-f",
-    "--firstrun",
-    action="store_true",
-    dest="firstrun",
-    help="Executes with 'first run' parameters (gets results from last 24h)",
-    default=False
-)
+# Main Methods
+def create_parser_options():
+    """
+    Adds various command-line arguments supported by the processing script.
+    """
+    parser.add_option(
+        "-t",
+        "--threads",
+        action="store",
+        dest="threads",
+        help="The number of worker threads to use while processing results.",
+        default=4,
+        type=int
+    )
+    parser.add_option(
+        "-f",
+        "--firstrun",
+        action="store_true",
+        dest="firstrun",
+        help="Executes with 'first run' parameters (gets results from last 24h)",
+        default=False
+    )
+    parser.add_option(
+        "-m",
+        "--minute",
+        action="store",
+        dest="hourly_min",
+        help="The minute when the hourly processor executes",
+        default=1,
+        type=int
+    )
 
-parser.add_option(
-    "-m",
-    "--minute",
-    action="store",
-    dest="hourly_min",
-    help="The minute when the hourly processor executes",
-    default=1,
-    type=int
-)
 
-
-# Methods
 def check_options():
+    """
+    Validates the command-line arguments provided by the user.
+
+    Raises:
+        SystemExit: If option is provided invalid value, print error and exit
+    """
     threads_ok = options.threads >= 1
     hour_min_ok = 0 <= options.hourly_min <= 59
 
@@ -79,7 +88,6 @@ def process_task(first_run=False):
             collect results last seen from current time - FIRST_RUN_HOURS
             instead of 1 hour.
     """
-
     results = {res['event_id']: res for res in bp_utils.query_badpackets(bp_api, first_run)}
     res_len = len(results)
 
@@ -102,7 +110,6 @@ def init_processing_loop():
     a KeyboardInterrupt is detected. Each loop sleeps for 1 second
     and the processing task executes once per hour at: HH::hourly_min:00
     """
-
     while True:
         dtnow = datetime.utcnow()
 
@@ -114,9 +121,9 @@ def init_processing_loop():
 
 
 if __name__ == "__main__":
+    create_parser_options()
     options, args = parser.parse_args()
     check_options()
-
     clear()
 
     print("Initialised VisIBoT BadPackets Pre-processor 🤖")
