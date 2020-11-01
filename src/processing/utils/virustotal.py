cc6ab0ff682b66277f1c8f7ab5275829b5ccf98b
Daniel A <den.xyz@pm.me>
2020-11-01 16:05:21 +0000

    Added VirusTotalURLProcessor utility class

--- src/processing/utils/virustotal.py
+++ src/processing/utils/virustotal.py
@@ -1,21 +1,77 @@
 from virus_total_apis import PublicApi as VirusTotalPublicApi
-import os
-# import json
+import time
 
-"""
-VirusTotal Public API
+class VirusTotalURLProcessor:
 
-Rate Limit: 4 requests/min
+    def __init__(self, api_key):
+        self.api = VirusTotalPublicApi(api_key)#
+        self.queue = []
+        self.current_url = None
+        self.running = True
 
-TODO: Look into: https://pypi.org/project/ratelimit/
-"""
 
-API_KEY = os.getenv("VIRUSTOTAL_API_KEY")
+    def init(self):
+        print("Starting VT URL Processor...")
+        while self.running:
+            if self.queue:
+                self.current_url = self.queue.pop()
+                print("Processing:", self.current_url)
+                self.process_url()
 
-MALWARE_URL = 'http://205.185.115.72/c'
+            time.sleep(0.5)
 
-vt = VirusTotalPublicApi(API_KEY)
 
-# response = vt.scan_url(MALWARE_URL)
-# response = vt.get_url_report(MALWARE_URL)
-# print(json.dumps(response, sort_keys=False, indent=4))
+    def stop(self):
+        self.runing = False
+        print("Stopping VT URL Processor...")
+
+
+    def add_job(self, url):
+        self.queue.append(url)
+
+
+    def hit_rate_limit(self, response):
+        limit_reached = response['response_code'] == 204
+
+        if limit_reached:
+            print("Hit rate limit... Sleeping 15 seconds")
+            time.sleep(15)
+
+        return limit_reached
+
+
+    def store_result(self, response):
+        # result = response['results']
+        print("TODO: Store data")
+
+
+    def handle_invalid_url(self):
+        print("TODO: Handle Invalid URL")
+
+
+    def scan_url(self):
+        response = self.api.scan_url(self.current_url)
+
+        if not self.hit_rate_limit(response):
+            if 'results' in response and response['results']['response_code'] == 1:
+                self.store_result(response)
+            else:
+                self.handle_invalid_url()
+        else:
+            self.scan_url()
+
+
+    def process_url(self):
+        response = self.api.get_url_report(self.current_url)
+
+        if not self.hit_rate_limit(response):
+            if 'results' in response:
+                if response['results']['response_code'] == 0:
+                    return self.scan_url()
+                elif response['results']['response_code'] == 1:
+                    return self.store_result(response)
+            self.handle_invalid_url()
+        else:
+            self.process_url()
+
+
