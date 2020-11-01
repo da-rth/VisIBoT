d39f271d04471430e3a1b3dc9b94ba0640cb7566
Daniel A <den.xyz@pm.me>
2020-11-01 17:04:38 +0000

    Modified VirusTotalURLPRocessor to use backoff and ratelimit modules for VT Api rate limiting

--- src/processing/utils/virustotal.py
+++ src/processing/utils/virustotal.py
@@ -1,77 +1,61 @@
 from virus_total_apis import PublicApi as VirusTotalPublicApi
-import time
+from ratelimit import limits, RateLimitException
+from backoff import on_exception, expo
+import time, os
 
 class VirusTotalURLProcessor:
 
     def __init__(self, api_key):
-        self.api = VirusTotalPublicApi(api_key)#
-        self.queue = []
-        self.current_url = None
-        self.running = True
+        self.api = VirusTotalPublicApi(api_key)
+        self.api_services = {
+            "scan": self.api.scan_url,
+            "check": self.api.get_url_report,
+        }
 
 
-    def init(self):
-        print("Starting VT URL Processor...")
-        while self.running:
-            if self.queue:
-                self.current_url = self.queue.pop()
-                print("Processing:", self.current_url)
-                self.process_url()
+    def store_result(self, response, processing=False):
+        result = response['results']
 
-            time.sleep(0.5)
+        res = {
+            "scan_url": result['permalink'],
+            "scan_id": result['scan_id'],
+            "scan_date": result['scan_date'],
+            "processing": processing,
+        }
 
+        if not processing:
+            res["positives"] = result["positives"]
+            res["total"] = result["total"]
 
-    def stop(self):
-        self.runing = False
-        print("Stopping VT URL Processor...")
-
-
-    def add_job(self, url):
-        self.queue.append(url)
+        return res
 
 
-    def hit_rate_limit(self, response):
-        limit_reached = response['response_code'] == 204
+    @on_exception(expo, RateLimitException)
+    @limits(calls=4, period=20)
+    def api_request(self, req_type, url):
+        response = self.api_services[req_type](url)
 
-        if limit_reached:
-            print("Hit rate limit... Sleeping 15 seconds")
-            time.sleep(15)
-
-        return limit_reached
-
-
-    def store_result(self, response):
-        # result = response['results']
-        print("TODO: Store data")
+        if response['response_code'] == 204:
+            raise RateLimitException("VirusTotal Rate Limit Exceeded", 60)
+        
+        return response
 
 
-    def handle_invalid_url(self):
-        print("TODO: Handle Invalid URL")
+    def scan_url(self, url):
+        response = self.api_request("scan", url)
+
+        if 'results' in response and response['results']['response_code'] == 1:
+            return self.store_result(response, processing=True)
+        return False
 
 
-    def scan_url(self):
-        response = self.api.scan_url(self.current_url)
+    def process_url(self, url):
+        response = self.api_request("check", url)
 
-        if not self.hit_rate_limit(response):
-            if 'results' in response and response['results']['response_code'] == 1:
-                self.store_result(response)
-            else:
-                self.handle_invalid_url()
-        else:
-            self.scan_url()
+        if 'results' in response:
+            if response['results']['response_code'] == 0:
+                return self.scan_url(url)
+            elif response['results']['response_code'] == 1:
+                return self.store_result(response, processing=False)
 
-
-    def process_url(self):
-        response = self.api.get_url_report(self.current_url)
-
-        if not self.hit_rate_limit(response):
-            if 'results' in response:
-                if response['results']['response_code'] == 0:
-                    return self.scan_url()
-                elif response['results']['response_code'] == 1:
-                    return self.store_result(response)
-            self.handle_invalid_url()
-        else:
-            self.process_url()
-
-
+        return False
