d397af45a7c00fa9660ea29c664dbe83198562a5
Daniel A <den.xyz@pm.me>
2020-11-01 17:14:41 +0000

    Add documentation for VirusTotalURLProcessor

--- src/processing/utils/virustotal.py
+++ src/processing/utils/virustotal.py
@@ -13,6 +13,19 @@ class VirusTotalURLProcessor:
         }
 
     def store_result(self, response, processing=False):
+        """
+        Given a response, return data in reduced form.
+        Only add fields "positives" and "total" if result is done processing.
+
+        Args:
+            response (dict): The VT response dict to be reduced
+            processing (bool, optional):
+                Specifies if the result is currently being processed by VT.
+                Defaults to False.
+
+        Returns:
+            dict: The response dict in reduced form
+        """
         result = response['results']
 
         res = {
@@ -29,8 +42,22 @@ class VirusTotalURLProcessor:
         return res
 
     @on_exception(expo, RateLimitException)
-    @limits(calls=4, period=20)
+    @limits(calls=4, period=30)
     def api_request(self, req_type, url):
+        """
+        Allows for various VT API calls to be made while ensuring
+        rate limit is respected. If rate limit occurs, try again in 30 seconds.
+
+        Args:
+            req_type (str): The type of VT request
+            url (str): The URL to be checked by VirusTotal
+
+        Raises:
+            RateLimitException: When number of requests exceeds rate limit
+
+        Returns:
+            dict: The response object from the VT endpoint
+        """
         response = self.api_services[req_type](url)
 
         if response['response_code'] == 204:
@@ -39,6 +66,16 @@ class VirusTotalURLProcessor:
         return response
 
     def scan_url(self, url):
+        """
+        Scans a URL using rate-limited VT API endpoints and returns result.
+
+        Args:
+            url (str): The URL to be scanned by VirusTotal
+
+        Returns:
+            dict: The results from request in reduced dict form
+            bool: False if no results are obtained from query
+        """
         response = self.api_request("scan", url)
 
         if 'results' in response and response['results']['response_code'] == 1:
@@ -46,6 +83,18 @@ class VirusTotalURLProcessor:
         return False
 
     def process_url(self, url):
+        """
+        Checks for an existing report for a URL using rate-limited VT API endpoints.
+        If report exists, return result data in dict form.
+        Otherwise, get report of URL and store results.
+
+        Args:
+            url (str): The URL to be scanned by VirusTotal
+
+        Returns:
+            bool: Returns False if no result is obtained (e.g. bad URL format)
+            dict: Returns the results of the report or scan in reduced dict form
+        """
         response = self.api_request("check", url)
 
         if 'results' in response:
