e1d4054445553c4ab685ca05dbf71fc23faa3fd5
denBot <den.xyz@pm.me>
2020-12-16 14:47:13 +0000

    Added private IP + blacklisted IP checking

--- src/processing/utils/lisa.py
+++ src/processing/utils/lisa.py
@@ -1,6 +1,11 @@
 import requests
 import threading
 import time
+import pydnsbl
+from IPy import IP
+
+
+ip_checker = pydnsbl.DNSBLIpChecker()
 
 
 class LiSaAPI:
@@ -35,12 +40,27 @@ class LiSaAPI:
             payload.vt_result = analysis['virustotal']
             payload.vt_result['processing'] = False
             payload.save()
+
+            candidate_servers = []
+            
+
+            for endpoint in endpoints_out:
+                ip_address = IP(endpoint['ip'])
+
+                if ip_address.iptype() == 'PRIVATE':
+                    continue
+                elif endpoint['blacklisted']:
+                    candidate_servers,append(endpoint)
+                elif ip_checker.check(str(ip_address)).blacklisted
+                    candidate_servers,append(endpoint)
+                elif endpoint['data_in'] > 0:
+                    candidate_servers,append(endpoint)
             # get payload from db
             # Store virustotal results in payload record
             # parse endpoints
             # parse strings for IP addresses
             # print(json.dumps(virus_total, indent=1))
-            print(f"success | strings: {len(strings)} | endponts: {len(endpoints_out)}")
+            print(f"success | strings: {len(strings)} | candidate servers: {len(candidate_servers)}")
 
     def init_task_checker(self):
         while True:
