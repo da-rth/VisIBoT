b5b332646e395c06273706dcc5cf65c74c5218b9
denBot <den.xyz@pm.me>
2021-02-01 20:27:36 +0000

    Updated .env.example for processing and web app

--- src/processing/worker/utils/tor_session.py
+++ src/processing/worker/utils/tor_session.py
@@ -1,7 +1,6 @@
 import os
 import requests
 
-USE_TOR = os.getenv("TOR_SESSION_ENABLE", False) == "True"
 TOR_URL = "socks5h://tor:9050"
 
 
@@ -19,14 +18,11 @@ def get_session():
 def check_session():
     session = get_session()
 
-    if USE_TOR:
-        print("[Tor Session] Proxy set to", session.proxies['http'])
-        try:
-            if "Congratulations" in session.get("https://check.torproject.org").text:
-                print("[Tor Session] Proxy successfully connected to Tor network.")
-            else:
-                print("[Tor Session] Proxy test failed. Is tor running?")
-        except Exception:
-            raise SystemExit("[Tor Session] Could not reach test website using tor session. Is tor running?")
-    else:
-        print("[Tor Session] Proxy session disabled. Using a proxyless (unprotected) session instead.")
+    print("[Tor Session] Proxy set to", session.proxies['http'])
+    try:
+        if "Congratulations" in session.get("https://check.torproject.org").text:
+            print("[Tor Session] Proxy successfully connected to Tor network.")
+        else:
+            print("[Tor Session] Proxy test failed. Is tor running?")
+    except Exception:
+        raise SystemExit("[Tor Session] Could not reach test website using tor session. Is tor running?")
