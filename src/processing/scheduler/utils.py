3f095c603964c7e039081d9992881110138bee8c
denBot <den.xyz@pm.me>
2021-02-02 14:19:39 +0000

    Removed tld-cache from repository

--- src/processing/scheduler/utils.py
+++ src/processing/scheduler/utils.py
@@ -4,8 +4,8 @@ from datetime import datetime
 def time_until(next_mins: int) -> str:
     """
     Calculates the timestamp at which the specified minute number will next occur.
-    
-    Example: 
+
+    Example:
     - if the time is 12:03:00 and next_mins=15, time_until(15) will produce the timestamp 12:15:00
     - if the time is 12:45:30 and next_mins=15, time_until(15) will produce the timestamp 13:15:00
 
