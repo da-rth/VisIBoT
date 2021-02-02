9acc6826968ecb13a196aee44afc721cd9d0677f
denBot <den.xyz@pm.me>
2021-02-02 14:16:09 +0000

    Added documentaiton to badpackets session.py

--- src/processing/scheduler/badpackets/session.py
+++ src/processing/scheduler/badpackets/session.py
@@ -3,6 +3,11 @@ from urllib.parse import urljoin
 
 
 class BadPacketsSession(Session):
+    """
+    Generates a session used for BadPackets API requests.
+    - The provided api_token is inserted into the header of each request made.
+    - All request url strings are prepended with the BadPackets API base url.
+    """
     def __init__(self, api_url=None, api_token=None, *args, **kwargs):
         super(BadPacketsSession, self).__init__(*args, **kwargs)
         self.api_url = api_url
