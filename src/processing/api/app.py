068bac4b8ce30cf836d7ef5a46bd61743fefa4f3
denBot <den.xyz@pm.me>
2021-02-02 13:46:16 +0000

    Added documentation for processing/api/app.py

--- src/processing/api/app.py
+++ src/processing/api/app.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 
 @app.route('/api/lisa-analysis/success/<string:task_id>', methods=['POST'])
 def analysis_complete(task_id: str) -> str:
+    """ 
+    On POST, a lisa task_id is parsed form the route URL and response.data is checked for analysis results.
+    If analysis data is present, a celery task is created to handle the processing of the LiSa analysis 
+    result data for the corresponding MalwarePayload.
+
+    Args:
+        task_id (str): The LiSa task_id referencing to a specific MalwarePayload
+
+    Returns:
+        str: Success/failure response message
+    """
     analysis_data = request.json
 
     if analysis_data:
@@ -18,7 +29,18 @@ def analysis_complete(task_id: str) -> str:
 
 
 @app.route('/api/lisa-analysis/failure/<string:task_id>', methods=['POST'])
-def analysis_failed(task_id: str) -> str:
+def analysis_failed(task_id: str) -> BaseResponse:
+    """ 
+    On POST, a lisa task_id is parsed form the route URL and response.data is checked for failure data.
+    If failure data is present, a celery task is created to handle the failed LiSa analysis attempt 
+    for the corresponding MalwarePayload.
+
+    Args:
+        task_id (str): The LiSa task_id referencing a specific MalwarePayload
+
+    Returns:
+        str: Success/failure response message
+    """
     failure_data = request.json
 
     if failure_data:
@@ -30,6 +52,12 @@ def analysis_failed(task_id: str) -> str:
 
 @app.route('/api/ping')
 def health_check() -> str:
+    """
+    Used as a health-check end-point to verify if the Flask application is running.
+
+    Returns:
+        str: Responds with HTTP Status Code 200 if Flask server is online.
+    """
     return jsonify("OK"), 200
 
 
--- src/processing/api/worker.py
+++ src/processing/api/worker.py
@@ -5,5 +5,4 @@ from celery import Celery
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 
-
 celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
