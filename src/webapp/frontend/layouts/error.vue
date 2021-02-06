29b3768359ed90a6ff73fa5cacb650ceb92ef411
denBot <den.xyz@pm.me>
2021-02-06 15:12:14 +0000

    Styled 404 page and added initial translation text to component

--- src/webapp/frontend/layouts/error.vue
+++ src/webapp/frontend/layouts/error.vue
@@ -1,9 +1,25 @@
 <template>
   <client-only>
-    <div>
-      <h1 v-if="error.statusCode === 404">Page not found</h1>
-      <h1 v-else>An error occurred</h1>
-      <NuxtLink to="/">Home page</NuxtLink>
+    <div
+      :class="{
+        errorContainerDark: !lightThemeEnabled,
+        errorContainerLight: lightThemeEnabled,
+      }"
+      class="p-4 errorContainer"
+    >
+      <b-jumbotron
+        :bg-variant="lightThemeEnabled ? 'light' : 'dark'"
+        :text-variant="lightThemeEnabled ? 'dark' : 'light'"
+        :header="$t('Whoops!')"
+        :lead="bodyMessage"
+      >
+        <div class="m-5">
+          <b-icon-emoji-dizzy font-scale="12" />
+        </div>
+        <b-button variant="primary" to="/">
+          {{ $t("Back to homepage") }}
+        </b-button>
+      </b-jumbotron>
     </div>
   </client-only>
 </template>
@@ -13,5 +29,33 @@ export default {
   layout: "error",
   // eslint-disable-next-line vue/require-prop-types
   props: ["error"],
+  computed: {
+    lightThemeEnabled() {
+      return this.$store.state.settings.lightThemeEnabled
+    },
+    bodyMessage() {
+      return this.error.statusCode === 404
+        ? this.$t("The page you are looking for could not be found.")
+        : this.$t("An error has occurred. Please try again later.")
+    },
+  },
 }
 </script>
+
+<style>
+.errorContainer {
+  text-align: center;
+  position: absolute;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  top: 0;
+  user-select: none !important;
+}
+.errorContainerDark {
+  background: #242424 !important;
+}
+.errorContainerLight {
+  background: #e9e9e9 !important;
+}
+</style>
